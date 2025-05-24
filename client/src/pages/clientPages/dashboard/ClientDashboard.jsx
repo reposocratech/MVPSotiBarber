aace8e29285b780abf07849ee266f3d23e5a841c
Adrian-SJ92 <adrian.sanchezj92@gmail.com>
2025-05-24 18:48:47 +0200

    actualizacion dashboard

--- client/src/pages/clientPages/dashboard/ClientDashboard.jsx
+++ client/src/pages/clientPages/dashboard/ClientDashboard.jsx
@@ -1,11 +1,10 @@
-import CompleteRegister from '../../../components/completeRegister/CompleteRegister'
+import CompleteRegister from '../../../components/completeRegister/CompleteRegister';
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../context/AuthContextProvider';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { UserIcon } from '../../../components/userIcon/UserIcon';
-import './clientDashboard.css'
+import './clientDashboard.css';
 import { useNavigate } from 'react-router-dom';
-
 
 const ClientDashboard = () => {
   const { user } = useContext(AuthContext);
@@ -14,17 +13,13 @@ const ClientDashboard = () => {
   console.log('+++++++++', user);
 
   return (
+    <>
+      <div>
+        {/* aqui se tendria que cargar el modal de completa tus datos primero (pero solo si los datos estan vacíos) */}
+        <CompleteRegister />
+        perfil del cliente
+      </div>
 
-    <div>
-      {/* aqui se tendria que cargar el modal de completa tus datos primero (pero solo si los datos estan vacíos) */}
-      <CompleteRegister />
-      perfil del cliente
-    </div>
-  ) 
-} 
-
-    
- 
       <Container>
         <section>
           <Row>
@@ -37,38 +32,43 @@ const ClientDashboard = () => {
           </Row>
         </section>
         <section>
-          <Row className='mb-5 align-items-stretch'>
+          <Row className="mb-5 align-items-stretch">
             <Col>
-              <div className='client-profile d-flex gap-4 justify-content-center align-items-center'>
-                <div className='d-flex align-items-center '>
+              <div className="client-profile d-flex gap-4 justify-content-center align-items-center">
+                <div className="d-flex align-items-center ">
                   <div>
-                    <UserIcon/>
+                    <UserIcon />
                   </div>
                   <div>
-                    <h3>{user.user_name} {user.lastname}</h3>
+                    <h3>
+                      {user.user_name} {user.lastname}
+                    </h3>
                     <span>{user.phone}</span>
                   </div>
                 </div>
 
                 <div>
-                  <Button className='button' onClick={()=>navigate('/client/editClient')} >Editar</Button>
+                  <Button
+                    className="button"
+                    onClick={() => navigate('/client/editClient')}
+                  >
+                    Editar
+                  </Button>
                 </div>
-
               </div>
             </Col>
             <Col>
-            <div className='history'>
-              <h3>Historial</h3>
-              <span>Tu historial de servicios</span>
-              <Button className='button'>Citas</Button>
-            </div>
+              <div className="history">
+                <h3>Historial</h3>
+                <span>Tu historial de servicios</span>
+                <Button className="button">Citas</Button>
+              </div>
             </Col>
-            </Row> 
+          </Row>
         </section>
       </Container>
-   
+    </>
   );
 };
-
 
 export default ClientDashboard;
