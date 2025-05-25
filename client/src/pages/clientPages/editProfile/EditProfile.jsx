3a7b0cf9f2b2b21362c8463437d16d64c3e66bdc
Adrian-SJ92 <adrian.sanchezj92@gmail.com>
2025-05-25 10:24:08 +0200

    editClient

--- client/src/pages/clientPages/editProfile/EditProfile.jsx
+++ client/src/pages/clientPages/editProfile/EditProfile.jsx
@@ -3,43 +3,56 @@ import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
 import './editProfile.css';
 import { AuthContext } from '../../../context/AuthContextProvider';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { fetchData } from '../../../helpers/axiosHelpers';
 
 const EditProfile = () => {
-  
-   const [imagenPreview, setImagenPreview] = useState(null);
-   const [archivo, setArchivo] = useState(null);
-   const {user, setUser} = useContext(AuthContext);
-   const [editData, setEditData] = useState(user);
-  
-   console.log("editdataaa", editData);
-   
+  const [imagenPreview, setImagenPreview] = useState(null);
+  const { user, setUser, token } = useContext(AuthContext);
+  const [editData, setEditData] = useState(user);
+  const [file, setFile] = useState();
 
-   const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if(file){
-      setArchivo(file)
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setImagenPreview(reader.result);
-      } ;
-      reader.readAsDataURL(file)
+  console.log("----------", token);
+  
+
+  const navigate = useNavigate();
+
+  console.log('editdataaa', editData);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditData({ ...editData, [name]: value });
+  };
+
+  const cancelEdit = () => {
+    setEditData(user);
+    navigate('/client');
+  };
+
+  const handleChangeFile = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const onSubmit = () => {
+    try {
+      const newFormData = new FormData();
+
+      newFormData.append('editData', JSON.stringify(editData));
+      newFormData.append('file', file);
+      let res = fetchData("client/editClient", "put", newFormData, {Authorization: `Bearer ${token}`});
+
+      console.log("reeeeeeeesssss", res);
+      
+
+      setUser(editData);
+      navigate('/client');
+    } catch (error) {
+
     }
-   }
+  };
 
-   const guardarArchivo = (e) => {
-    console.log('imagen lista para subir', archivo);
-    
-   }
-
-   const handleChange = (e) => {
-     const {name, value} = e.target;
-     setEditData({...editData, [name]: value})
-   }
-  
   return (
-
-  
-
     <section className="edit-user">
       <Container>
         <Row className="engloba">
@@ -52,7 +65,7 @@ const EditProfile = () => {
                   name="user_name"
                   placeholder="Enter your name"
                   onChange={handleChange}
-                  value={editData.user_name? editData.user_name : ""}
+                  value={editData.user_name ? editData.user_name : ''}
                 />
               </Form.Group>
 
@@ -63,7 +76,7 @@ const EditProfile = () => {
                   name="lastname"
                   placeholder="Enter your lastname"
                   onChange={handleChange}
-                  value={editData.lastname? editData.lastname : ""}
+                  value={editData.lastname ? editData.lastname : ''}
                 />
               </Form.Group>
 
@@ -71,12 +84,13 @@ const EditProfile = () => {
                 <Form.Label htmlFor="NameInput">
                   Fecha de nacimiento:
                 </Form.Label>
-                <Form.Control 
-                    id="DateImput" 
-                    type="date" 
-                    name="birth_date"
-                    onChange={handleChange}
-                    value={editData.birth_date? editData.birth_date : ""} />
+                <Form.Control
+                  id="DateImput"
+                  type="date"
+                  name="birth_date"
+                  onChange={handleChange}
+                  value={editData.birth_date ? editData.birth_date : ''}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3">
@@ -86,7 +100,7 @@ const EditProfile = () => {
                   name="phone"
                   placeholder="Enter your phone number"
                   onChange={handleChange}
-                  value={editData.phone? editData.phone : ""}
+                  value={editData.phone ? editData.phone : ''}
                 />
               </Form.Group>
 
@@ -95,11 +109,16 @@ const EditProfile = () => {
                 <Form.Control
                   id="ImageImput"
                   name="avatar"
-                  type='file'
+                  type="file"
+                  onChange={handleChangeFile}
                 />
               </Form.Group>
-
-              <Button type="submit">Editar</Button>
+              <div className="d-flex gap-2 justify-content-center">
+                <Button onClick={onSubmit}>Editar</Button>
+                <Button type="button" onClick={cancelEdit}>
+                  Cancelar
+                </Button>
+              </div>
             </Form>
           </Col>
         </Row>
