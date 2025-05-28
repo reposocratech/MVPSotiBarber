024aae7c677259f16a2f8108ca51a8e965f786ac
javi <javisanchez800@gmail.com>
2025-05-28 08:31:50 +0200

    EmployeeClientV3

--- client/src/pages/employeePages/clientList/ClientList.jsx
+++ client/src/pages/employeePages/clientList/ClientList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Row, Col, Form, Button } from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
+import userIcon from '../../../assets/icons/userlogo.png';
 import "./clientlist.css";
 
 const ClientList = () => {
@@ -37,48 +38,60 @@ const ClientList = () => {
   );
 
   return (
-    <section>
-  <div className="d-flex flex-column align-items-center justify-content-center py-4">
-    <h3>Clientes</h3>
-    <div className="blue-line"></div>
-  </div>
+    <section className="padding y section">
+      <div className="d-flex flex-column align-items-center justify-content-center py-4">
+        <h3>Clientes</h3>
+        <div className="blue-line"></div>
+      </div>
 
-  <Row>
-    {clients.length !== 0 ? (
-      <Col className="d-flex justify-content-center align-items-center">
-        <div className="table-clients d-flex flex-column align-items-center justify-content-center">
-          <div className="search-bar-wrapper mb-4 w-100 d-flex justify-content-center">
-            <Form.Control
-              type="text"
-              placeholder="Buscar"
-              className="search-input"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-          </div>
-          <div className="table-scroll-wrapper">
-            <table>
-              <tbody>
-                {filteredClients.map((client) => (
-                  <tr key={client.user_id}>
-                    <td>{client.user_name} {client.lastname}</td>
-                    <td>
-                      <Button className="btn" onClick={() => handleViewProfile(client.user_id)}>Ver más</Button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </Col>
-    ) : (
-      <Col className="text-center">
-        <p>No hay clientes registrados.</p>
-      </Col>
-    )}
-  </Row>
-</section>
+      <Row>
+        {clients.length !== 0 ? (
+          <Col>
+            <div className="table-clients">
+              <div className="search-bar-wrapper mb-4">
+                <Form.Control
+                  type="text"
+                  placeholder="Buscar"
+                  className="search-input"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+              </div>
+              <div className="table-scroll-wrapper">
+                <table>
+                  <tbody>
+                    {filteredClients.map((client) => (
+                      <tr key={client.user_id}>
+                        <td>
+                          {client.user_name} {client.lastname}
+                        </td>
+                        <td>
+                          <Button
+                            className="btn"
+                            onClick={() => handleViewProfile(client.user_id)}
+                          >
+                            <span className="btn-text">Ver más</span>
+                            <img
+                              src={userIcon}
+                              alt="Ver perfil"
+                              className="btn-icon"
+                            />
+                          </Button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </Col>
+        ) : (
+          <Col className="text-center">
+            <p>No hay clientes registrados.</p>
+          </Col>
+        )}
+      </Row>
+    </section>
   );
 };
 
